e7c79875f9abd4b75cd3198bb0f2d307d7aa2358
Gabriel Rejtman <rejtmangabriel@gmail.com>
2023-03-25 16:44:32 -0400

    Fixed modules

--- game.py
+++ game.py
@@ -2,69 +2,73 @@ from random import randint
 from config import *
 from player import Player
 from enemys import Enemy
-# from bullet import Bullet
-pygame.init()
+import pygame
 
 
-def draw(win, player, enemy_group, bullet_group):
-    win.fill(BLACK)
-    pygame.draw.rect(win, (255, 0, 0), (0, 50, WIDTH, HEIGHT - 50), 5)
-    bullet_group.draw(win)
-    player.draw(win)
-    for curr_enemy in enemy_group:
-        curr_enemy.draw(win)
-    pygame.display.update()
+class Game(pygame.sprite.Sprite):
+    def __init__(self, screen):
+        super().__init__()
+        self.screen = screen
+        self.enemy_group = pygame.sprite.Group()
+        self.player = Player("player_img_front", WIDTH // 2, HEIGHT // 2, player_height, player_width)
+        self.bullet_group = pygame.sprite.Group()
+        self.run = True
+        self.menu = False
+        self.clock = pygame.time.Clock()
 
+        for cnt in range(randint(1, 10)):
+            enemy = Enemy("enemy_img.png", randint(0, WIDTH), randint(50, HEIGHT))
+            self.enemy_group.add(enemy)
 
-def run_menu(menu):
-    while menu:
-        win.fill(BLACK)
-        win.blit(play_img, (WIDTH // 2, (HEIGHT - 50) // 2))
+    def draw(self):
+        self.screen.fill(BLACK)
+        pygame.draw.rect(self.screen, (255, 0, 0), (0, 50, WIDTH, HEIGHT - 50), 5)
+        self.bullet_group.draw(self.screen)
+        self.player.draw(self.screen)
+        for curr_enemy in self.enemy_group:
+            curr_enemy.draw(self.screen)
+        pygame.display.update()
 
+    def run_menu(self, menu):
+        while menu:
+            self.screen.fill(BLACK)
+            self.screen.blit(play_img, (WIDTH // 2, (HEIGHT - 50) // 2))
 
-def main():
-    run = True
-    menu = False
-    clock = pygame.time.Clock()
+    def main_loop(self):
+        while not self.menu:
+            while self.run:
+                self.clock.tick(FPS)
+                self.draw()
+                for event in pygame.event.get():
+                    if event.type == pygame.QUIT:
+                        self.run = False
+                        break
+                    if event.type == pygame.MOUSEBUTTONDOWN:
+                        self.bullet_group.add(self.player.create_bullet())
 
-    # run_menu(menu)
-
-    enemy_group = pygame.sprite.Group()
-    player = Player("player_img_front", WIDTH // 2, HEIGHT // 2, player_height, player_width)
-    for cnt in range(randint(1, 10)):
-        enemy = Enemy("enemy_img.png", randint(0, WIDTH), randint(50, HEIGHT))
-        enemy_group.add(enemy)
-
-    bullet_group = pygame.sprite.Group()
-    while not menu:
-        while run:
-            clock.tick(FPS)
-            draw(win, player, enemy_group, bullet_group)
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    run = False
-                    break
-                if event.type == pygame.MOUSEBUTTONDOWN:
-                    bullet_group.add(player.create_bullet())
-
-                    for curr_bullet in bullet_group:
-                        if keys[pygame.K_w]:
-                            curr_bullet.move_y(up=True)
-                        if keys[pygame.K_s]:
-                            curr_bullet.move_y(up=False)
-                        if keys[pygame.K_d]:
-                            curr_bullet.move_x(right=True)
-                        if keys[pygame.K_a]:
-                            curr_bullet.move_x(right=False)
-            gets_hit = pygame.sprite.spritecollideany(player, enemy_group)
-            print(gets_hit)
-            keys = pygame.key.get_pressed()
-            player.player_movement(keys, player, bullet_group)
-            player.player_rotation(keys, player, win, player.x, player.y)
-            enemy.enemy_movement(enemy_group, player)
-            bullet_group.update()
-        pygame.quit()
+                        for curr_bullet in self.bullet_group:
+                            keys = pygame.key.get_pressed()
+                            if keys[pygame.K_w]:
+                                curr_bullet.move_y(up=True)
+                            if keys[pygame.K_s]:
+                                curr_bullet.move_y(up=False)
+                            if keys[pygame.K_d]:
+                                curr_bullet.move_x(right=True)
+                            if keys[pygame.K_a]:
+                                curr_bullet.move_x(right=False)
+                gets_hit = pygame.sprite.spritecollideany(self.player, self.enemy_group)
+                print(gets_hit)
+                keys = pygame.key.get_pressed()
+                self.player.player_movement(keys, self.player, self.bullet_group)
+                self.player.player_rotation(keys, self.player, self.screen, self.player.x, self.player.y)
+                Enemy.enemy_movement(self.enemy_group, self.player)
+                self.bullet_group.update()
+            pygame.quit()
 
 
 if __name__ == '__main__':
-    main()
+    pygame.init()
+    screen = pygame.display.set_mode((WIDTH, HEIGHT))
+    game = Game(screen)
+    game.main_loop()
+    
---
+++ main.py
@@ -0,0 +1,7 @@
+import game
+from config import *
+
+# pygame.init()
+# run_menu(menu)
+# draw(win, player, enemy_group, bullet_group)
+# main()
