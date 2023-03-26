1296c749b1ae47fb24aa0f9b28d82253ccf983c6
William Matheus <108390581+willy346@users.noreply.github.com>
2023-03-25 23:12:59 -0400

    Add files via upload

--- config.py
+++ config.py
@@ -1,14 +1,31 @@
 import pygame
 
+# looping
+loop = True
+
 # Colors
 yellow = (255, 255, 0)
 
+# screen height and width
 screen_width = 1000
 screen_height = 650
-
+    
 proportion = 100
+
+# sets the game surface
+game_surface = pygame.Surface(([1000, 650]))
 
 border_width = 5
 
+# clock 
 clk = pygame.time.Clock()
 fps = 60
+
+# player/enemies/bullet speed
+player_speed = 7
+bullet_speed = 11
+shoot_cooldown = 7
+# sheets 
+player_sheet = pygame.image.load("sprites/playersheet.png")
+player = pygame.image.load("sprites/player.png")
+bullet = pygame.image.load("sprites/testeball.png")
--- game.py
+++ game.py
@@ -1,52 +1,81 @@
-import pygame
+from random import randint
 from config import *
-
-pygame.init()
-screen = pygame.display.set_mode((screen_width, screen_height))
-pygame.display.set_caption("Robotron")
+from enemy import Enemy
+from player import Player
+from layouts import Layouts
 
 # joysticks = [pygame.joystick.Joystick(x) for x in range(pygame.joystick.get_count())]
 # for joystick in joysticks:
 #    joystick.init()
 
-game_surface = pygame.Surface((screen_width - proportion, screen_height - proportion))
 
+class Game(pygame.sprite.Sprite):
+    def __init__(self, screen):
+        self.screen = screen
+        self.loop = loop
+        self.walls = Layouts().get_group()
+        self.player_sprites = pygame.sprite.Group()
+        self.player = Player((500, 300), player)
+        self.player_sprites.add(self.player)
+        self.background = game_surface
+        self.enemy_group = pygame.sprite.Group()
+        for i in range(randint(1, 10)):
+            enemy = Enemy(randint(0, screen_width), randint(50, screen_height))
+            self.enemy_group.add(enemy)
 
-def draw_borders():
-    global screen
-    pygame.draw.rect(screen, yellow, (proportion / 2 - border_width, proportion / 2 - border_width,
-                                      screen_width-proportion + (border_width*2), border_width))
-
-    pygame.draw.rect(screen, yellow, (proportion / 2 - border_width, screen_height - (proportion/2),
-                                      screen_width - proportion + (border_width * 2), border_width))
-
-    pygame.draw.rect(screen, yellow, ((proportion/2) - border_width, (proportion/2) - border_width,
-                                      border_width, screen_height - proportion + border_width))
-
-    pygame.draw.rect(screen, yellow, (screen_width - (proportion / 2), (proportion / 2) - border_width,
-                                      border_width, screen_height - proportion + border_width))
-
-
-class Game:
-    def __init__(self):
-        pass
 
     # Check if an event happens
-    @staticmethod
-    def check_events():
-        clk.tick(60)
+    def check_events(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 exit()
+            if pygame.key.get_pressed()[pygame.K_SPACE]:
+                self.player.shoot()
 
+    def bullet_collision(self):
+        # sets the collision between bullet and wall groups
+        for wall in self.walls:
+            for bullet in self.player.bullet_group:
+                if pygame.sprite.collide_mask(bullet, wall):
+                    bullet.kill()
+        # clear the ball list
+        self.player.bullet_list.clear()
+
+    def player_collision(self):
+        for wall in self.walls:
+            if pygame.sprite.collide_mask(self.player, wall):
+                if abs(self.player.rect.top - wall.rect.bottom) < 60:
+                    self.player.rect.y += player_speed
+                elif abs(wall.rect.top - self.player.rect.bottom) < 40:
+                    self.player.rect.y -= player_speed
+                elif abs(wall.rect.left - self.player.rect.right) < 74:
+                    self.player.rect.x -= player_speed
+                elif abs(self.player.rect.left - wall.rect.right) < 74:
+                    self.player.rect.x += player_speed
+
+    # sets the game looping
     def game_loop(self):
 
-        while True:
-            screen.blit(game_surface, (proportion / 2, proportion / 2))
-            draw_borders()
-
+        while self.loop:
+            self.screen.blit(game_surface, (0, 0))
             self.check_events()
+            self.draw_sprites()
+            self.player.move()
+            self.player_collision()
+            self.bullet_collision()
+            for curr_enemy in self.enemy_group:
+                curr_enemy.draw(self.screen)
+            Enemy.enemy_movement(self.enemy_group, self.player)
             pygame.display.update()
-
             clk.tick(fps)
+
+    # draw elements
+    def draw_sprites(self):
+        self.walls.draw(self.screen)
+        self.player.update()
+        self.player_sprites.draw(self.screen)
+        self.player.bullet_group.draw(self.screen)
+        self.player.bullet_group.update()
+        for curr_enemy in self.enemy_group:
+            curr_enemy.draw(self.screen)
